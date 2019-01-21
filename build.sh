66ee0405ef757c7c741e4e0f38742362e494dc35
Gianluca Bargelli <g.bargelli@gmail.com>
2019-01-22 00:46:24 +0100

    new docs

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cargo +nightly build --target wasm32-unknown-unknown --lib && \
-  wasm-bindgen target/wasm32-unknown-unknown/debug/emuchip_8.wasm --out-dir www/
+cargo +nightly build --target wasm32-unknown-unknown --release --lib && \
+  wasm-bindgen target/wasm32-unknown-unknown/release/emuchip_8.wasm --out-dir www/
